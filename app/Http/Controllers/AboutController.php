47ae281a56a23f9f1b5e5a8eaa51268ca3241d67
Fahridanaa <fahridanaaa@gmail.com>
2024-02-22 19:25:15 +0700

    Implement new controllers and update routing.

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return '2241720158 - Fahridana Ahmad Rayyansyah';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Selamat Datang";
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
 
-Route::get('/world', function () {
-    return 'World';
-});
 
-Route::get('/about', function () {
-    return "2241720158-Fahridana Ahmad Rayyansyah";
-});
+
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
@@ -36,10 +40,6 @@ Route::get('/user/{name?}', function ($name='John') {
 Route::get('/posts/{post}/comments/{comment}', function
 ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function ($articleId) {
-    return 'Halaman Artikel dengan ID '.$articleId;
 });
 
 
